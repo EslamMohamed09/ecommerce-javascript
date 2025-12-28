443a642fe659d82054b2612b6f4da786cfce9c40
EslamMohamed09 <eslammayo2022@gmail.com>
2025-12-29 01:02:05 +0200

    Add Best Sellers Section

--- inc/js/main.js
+++ inc/js/main.js
@@ -437,14 +437,14 @@ function filterWithTabs(tabs, Items) {
   });
 }
 
-filterWithTabs(document.querySelectorAll('#main-categories .category-panel1 .left-col .first-pcat-names h4'),
-               document.querySelectorAll('#main-categories .category-panel1 .right-col .product-item'));
+filterWithTabs(document.querySelectorAll('.main-categories-section .category-panel1 .left-col .first-pcat-names h4'),
+               document.querySelectorAll('.main-categories-section .category-panel1 .right-col .product-item'));
 
-filterWithTabs(document.querySelectorAll('#main-categories .category-panel2 .left-col .second-pcat-names h4'),
-               document.querySelectorAll('#main-categories .category-panel2 .right-col .product-item'));
+filterWithTabs(document.querySelectorAll('.main-categories-section .category-panel2 .left-col .second-pcat-names h4'),
+               document.querySelectorAll('.main-categories-section .category-panel2 .right-col .product-item'));
 
-filterWithTabs(document.querySelectorAll('#main-categories .category-panel3 .left-col .third-pcat-names h4'),
-               document.querySelectorAll('#main-categories .category-panel3 .right-col .product-ad'));
+filterWithTabs(document.querySelectorAll('.main-categories-section .category-panel3 .left-col .third-pcat-names h4'),
+               document.querySelectorAll('.main-categories-section .category-panel3 .right-col .product-ad'));
 
 
 // if(header){
