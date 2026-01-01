8c8411240169ef084827978b96b4316a00a8d3c5
EslamMohamed09 <eslammayo2022@gmail.com>
2026-01-02 00:55:56 +0200

    modify view count of items to best seller section

--- inc/js/main.js
+++ inc/js/main.js
@@ -1010,7 +1010,7 @@ function scrollSlider5Items(options) {
       { breakpoint: 730, settings: { slidesToShow: 3, slidesToScroll: 3 } },
       { breakpoint: 1000, settings: { slidesToShow: 4, slidesToScroll: 4 } },
       { breakpoint: 1300, settings: { slidesToShow: 5, slidesToScroll: 5 } },
-      { breakpoint: 1400, settings: { slidesToShow: 6, slidesToScroll: 6 } },
+      { breakpoint: 1490, settings: { slidesToShow: 6, slidesToScroll: 6 } },
       { breakpoint: 1600, settings: { slidesToShow: 7, slidesToScroll: 7 } }
     ];
 
